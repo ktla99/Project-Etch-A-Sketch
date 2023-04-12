4354f050877457aa303df755ad29018bdadd8891
Kim <ktla3223@gmail.com>
2023-04-12 13:12:42 -0700

    Added .classList.add to lines 4, 7, and 11. Spaced out each input by adding one line

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
 const containerOne = document.querySelector('#containerOne');
 
 const row = document.createElement('div');
+row.classList.add('row');
+
 const column = document.createElement('div');
+column.classList.add('column');
+
 const box = document.createElement('div');
+box.classList.add('box');
+
 containerOne.appendChild(row);
 containerOne.appendChild(column);
 containerOne.appendChild(box);
