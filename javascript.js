b77bc4ee1072bafe74a627ad35ef1fcfdff7bbb6
Kim <ktla3223@gmail.com>
2023-04-14 14:24:46 -0700

    Added space between grid.styles from lines 7-13

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 let container = document.querySelector('#container');
 
 function createGrid() {
-    for (let boxes = 0; boxes < (16*16); boxes ++) {
+    for(let boxes = 0; boxes < (16*16); boxes ++) {
         let grid = document.createElement('div');
         container.appendChild(grid);
+        
         grid.style.height = 50 + 'px';
         grid.style.width = 50 + 'px';
+        
         grid.style.flexShrink = '0';
+        
         grid.style.border = '1px solid blue';
     }
 }
