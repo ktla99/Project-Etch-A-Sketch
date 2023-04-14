07f0049b4d11c84c4ddd6d44126eab1ed11d73e7
Kim <ktla3223@gmail.com>
2023-04-14 13:27:06 -0700

    Added ability to move mouse over and leave behind black colors

--- javascript.js
+++ javascript.js
@@ -51,6 +51,9 @@ changeGridSize();
 
 let newGrid = document.querySelectorAll('#container > div');
 
-
-]
-}
+for(const div of newGrid) {
+    div.addEventListener('mouseover', function.mouseover() {
+        div.style.backgroundColor = 'black';
+    });
+    };
+};
