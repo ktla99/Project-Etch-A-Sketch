4e3dd995479fe0056f3b4be63101bab544d192e4
Kim <ktla3223@gmail.com>
2023-04-17 10:26:28 -0700

    Created function generateGrid with restraints on inputting numbers above 100 on lines 7-10

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+//Restart Etch-A-Sketch by removing everything
 function clearGrid() {
     let grid = document.querySelector(".grid");
+    grid.innerHTML = "";
 }
+
+function generateGrid(size = 16) {
+    clearGrid();
+    if (size > 100) {
+        alert("Please only put in numbers between 0-100!")
+    }
+    
